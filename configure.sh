a5802b800b8de8a5de691cbab09e2a9161c06f33
kaotisk <kaotisk@arching-kaos.com>
2021-08-18 22:57:12 +0300

    Added small/sample configuration script

---
+++ configure.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sed -i.bak -e 's/{$DOCS_SERVER_NAME}/docs.arching-kaos.local/g' generic.html
+sed -i.bak -e 's/{$ICECAST_SERVER_NAME}/icecast.arching-kaos.local/g' generic.html
+sed -i.bak -e 's/{$IPFS_SERVER_NAME}/ipfs.arching-kaos.local/g' generic.html
+sed -i.bak -e 's/{$IRC_SERVER_NAME}/irc.arching-kaos.local/g' generic.html
+sed -i.bak -e 's/{$RADIO_SERVER_NAME}/radio.arching-kaos.local/g' generic.html
+sed -i.bak -e 's/{$SSB_SERVER_NAME}/ssb.arching-kaos.local/g' generic.html
