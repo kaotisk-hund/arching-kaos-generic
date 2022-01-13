61b2353432a81a7a3b3c72d9216df30a26b18569
kaotisk <kaotisk@arching-kaos.com>
2022-01-13 03:47:12 +0200

    Added link for arching-kaos-explorer

--- configure.sh
+++ configure.sh
@@ -5,3 +5,4 @@ sed -i.bak -e 's/{$IPFS_SERVER_NAME}/ipfs.arching-kaos.local/g' generic.html
 sed -i.bak -e 's/{$IRC_SERVER_NAME}/irc.arching-kaos.local/g' generic.html
 sed -i.bak -e 's/{$RADIO_SERVER_NAME}/radio.arching-kaos.local/g' generic.html
 sed -i.bak -e 's/{$SSB_SERVER_NAME}/ssb.arching-kaos.local/g' generic.html
+sed -i.bak -e 's/{$EXPLORER_SERVER_NAME}/explorer.arching-kaos.local/g' generic.html
